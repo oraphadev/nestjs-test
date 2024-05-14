b40b33fa6c366507a9a2cd6ddaacc9fa50cf6b6a
Raphael Lima <oraphadev@gmail.com>
2024-05-14 11:36:48 -0300

    feat(master): ✨ remove hostname

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.APP_PORT, process.env.APP_HOST);
+  await app.listen(process.env.APP_PORT);
 }
 
 bootstrap();

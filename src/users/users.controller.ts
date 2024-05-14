c0fd35585ad242f81381920835c05816711b5e1e
Raphael Lima <oraphadev@gmail.com>
2024-05-14 11:17:34 -0300

    feat(master): ✨ remove users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -15,5 +15,10 @@ export class UsersController {
   @Post()
   async create(@Body() payload: CreateUserDto): Promise<User> {
     return this.usersService.create(payload);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string): Promise<User> {
+    return this.usersService.remove({ id });
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Prisma, User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 import { PrismaService } from '../prisma.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UserAlreadyExistsException } from './exceptions/user-already-exists.exception';
@@ -13,19 +14,28 @@ export class UsersService {
     return users;
   }
 
-  async create(payload: CreateUserDto): Promise<User> {
+  async create({ password, ...payload }: CreateUserDto): Promise<User> {
     const alreadyExists = await this.findOne({ email: payload.email });
     if (alreadyExists) {
       throw new UserAlreadyExistsException();
     }
     const user = await this.prisma.user.create({
-      data: payload,
+      data: {
+        ...payload,
+        password: bcrypt.hashSync(password, 10),
+      },
     });
     return user;
   }
 
   async findOne(where: Prisma.UserWhereUniqueInput): Promise<User | null> {
     return this.prisma.user.findUnique({
+      where,
+    });
+  }
+
+  async remove(where: Prisma.UserWhereUniqueInput): Promise<User> {
+    return this.prisma.user.delete({
       where,
     });
   }
